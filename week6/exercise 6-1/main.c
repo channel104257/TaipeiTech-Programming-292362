8b1a81bf2e26e4f4421a14e47e84fe914dbde20b
Channel <69780324+channel104257@users.noreply.github.com>
2021-10-30 15:20:10 +0800

    Update and rename 1.txt to main.c

---
+++ week6/exercise 6-1/main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#define RESPONSES_SIZE 15
+#define FREQUENCY_SIZE 10
+
+int responses[RESPONSES_SIZE];
+int rating[FREQUENCY_SIZE] = {0};
+
+int main()
+{
+    int score;
+
+    for (size_t i = 1; i <= RESPONSES_SIZE; ++i)
+    {
+        printf("Please Enter The Responses %d (1 ~ 10) : ", i);
+        scanf("%d", &score);
+
+        if (score < 1 || score > 10)
+        {
+            i = i - 1;
+        }
+
+        if (score >= 1 || score <= 10)
+        {
+            responses[i] = score;
+
+            for (size_t j = 1; j <= FREQUENCY_SIZE; j++)
+            {
+                if (j == score)
+                {
+                    rating[j]++;
+                }
+            }
+        }
+    }
+
+    printf("\nRating   Frequency\n");
+    for (size_t i = 1; i <= FREQUENCY_SIZE; i++)
+    {
+        printf("%6d%12d\n",i, rating[i]);
+    }
+
+    return 0;
+}
